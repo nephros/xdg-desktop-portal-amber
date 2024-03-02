c13f22b52528e8dc259e059e020072d681e9624d
nephros <nemo@pgxperiiia10>
2024-03-02 23:24:46 +0100

    Dont show view if prestart

--- qml/app.cpp
+++ qml/app.cpp
@@ -1,5 +1,24 @@
 #include <sailfishapp.h>
 
 int main(int argc, char **argv) {
-    return SailfishApp::main(argc, argv);
+
+    bool prestart = QCoreApplication::arguments().contains(QStringLiteral("--prestart"));
+
+    //return SailfishApp::main(argc, argv);
+    QQuickView* view = SailfishApp::createView();
+    view->setSource(SailfishApp::pathToMainQml());
+
+    /*
+    if (!QDBusConnection::sessionBus().registerObject("/org/freedesktop/impl/portal/desktop/amber/ui", view))
+        qWarning() << "Could not register D-Bus object.";
+
+    if (!QDBusConnection::sessionBus().registerService("org.freedesktop.impl.portal.desktop.amber.ui"))
+        qWarning() << "Could not register D-Bus service.";
+
+    */
+    if(!prestart)
+    {
+        view->show();
+    }
+    return app->exec();
 }

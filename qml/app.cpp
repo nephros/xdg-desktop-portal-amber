3dc8f3e6049bbae5c93f5c8576fb998708343f47
nephros <nemo@pgxperiiia10>
2024-03-03 08:55:48 +0100

    Register app name and dbus

--- qml/app.cpp
+++ qml/app.cpp
@@ -2,6 +2,8 @@
 #include <QDBusConnection>
 #include <sailfishapp.h>
 
+#include <QtDebug>
+
 int main(int argc, char **argv) {
 
     QGuiApplication* app = SailfishApp::application(argc, argv);
@@ -10,17 +12,21 @@ int main(int argc, char **argv) {
     QQuickView* view = SailfishApp::createView();
     view->setSource(SailfishApp::pathToMainQml());
 
-    /*
+    app->setOrganizationName(QStringLiteral("org.freedesktop.portal.desktop.amber"));
+    app->setApplicationName(QStringLiteral("ui"));
+    //app->setApplicationVersion(QStringLiteral(APP_VERSION));
+
     if (!QDBusConnection::sessionBus().registerObject("/org/freedesktop/impl/portal/desktop/amber/ui", view))
         qWarning() << "Could not register D-Bus object.";
 
     if (!QDBusConnection::sessionBus().registerService("org.freedesktop.impl.portal.desktop.amber.ui"))
         qWarning() << "Could not register D-Bus service.";
 
-    */
     if(!prestart)
     {
         view->show();
+    } else {
+        qDebug() << "Prestart, not showing UI yet.";
     }
     return app->exec();
 }

60141a025b4054c6378d5b449cd49f95eb210548
nephros <nemo@pgxperiiia10>
2024-03-11 17:51:51 +0100

    Use underscore because specifications

--- src/lockdown.h
+++ src/lockdown.h
@@ -17,24 +17,22 @@ class LockdownPortal : public QDBusAbstractAdaptor
 {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "org.freedesktop.impl.portal.Lockdown")
+    // note: the XDP spec uses hyphens as property names. They are not allowed
+    // in C++ identifiers, and deprecated for D-Bus names.
+    // soo, underscores.
+    Q_PROPERTY(bool disable_location     READ getGPS WRITE setGPS)
+    Q_PROPERTY(bool disable_sound_output READ getMute WRITE setMute)
 
 public:
     explicit LockdownPortal(QObject *parent);
     //~LockdownPortal() override;
 
-    //Q_PROPERTY(bool disable-printing  READ foo WRITE bar);
-    //Q_PROPERTY(bool disable-save-to-disk  READ foo WRITE bar);
-    //Q_PROPERTY(bool disable-application-handlers  READ foo WRITE bar);
-    Q_PROPERTY(bool disable-location     READ getGPS WRITE setGPS);
-    //Q_PROPERTY(bool disable-camera       READ foo WRITE bar);
-    //Q_PROPERTY(bool disable-microphone   READ getMic WRITE setMic);
-    Q_PROPERTY(bool disable-sound-output READ getMute WRITE setMute);
-
 public Q_SLOTS:
     bool getGPS() const;
     void setGPS(const bool &off) const;
     bool getMute() const;
-    void setMute(const bool &silent const) const;
+    void setMute(const bool &silent) const;
 
+};
 }
 #endif // XDG_DESKTOP_PORTAL_AMBER_LOCKDOWN_H
